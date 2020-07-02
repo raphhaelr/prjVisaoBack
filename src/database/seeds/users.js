9b654127c424b5b9818e05aedf96a4b81e72df61
Raphael Rodrigues <52205334+raphhaelr@users.noreply.github.com>
2020-07-02 10:13:56 -0300

    Arrumando problema das seeds

--- src/database/seeds/users.js
+++ src/database/seeds/users.js
@@ -1,3 +1,4 @@
+const { uuid } = require('uuidv4')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -5,9 +6,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {name: 'Raphael Rodrigues', type: 'admin', email: "raphael@email.com", password: "123456"},
-        {name: 'Nahan Rezende', type: 'user', email: "nahan@email.com", password: "123456"},
-
+        { id: uuid(), name: 'Raphael Rodrigues', type: 'admin', email: "raphael@email.com", password: "123456" },
+        { id: uuid(), name: 'Nahan Rezende', type: 'user', email: "nahan@email.com", password: "123456" },
       ]);
     });
 };
